1ad7be72603be676bf4a21f7c81b71e2300189a6
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-04-01 18:34:26 -0700

    Basic node structure has been created

--- src/Home.java
+++ src/Home.java
@@ -8,7 +8,7 @@ public class Home {
     //Call to static function for immutable example
 	//System.out.println(Immutable.Validate());
 		
-	
+	/*
 	//Call to a static function to reverse a phrase
 	System.out.println(ArrayFunctions.ReversePhrase("My Name is Prathap"));
 	
@@ -17,7 +17,54 @@ public class Home {
 	
 	//String to validate if it is chinese character or japaneese character
 	System.out.println(ArrayFunctions.ReversePhraseChinJap("ABCabc"));
+	*/
+		Node first = new Node();
+		
+		
+		/***
+		 * firstNode
+		 * 3 | null +
+		 */
+		first.setValue(3);
+		
+		/***
+		 * Middle Node
+		 * 3 | null + 5 | null 
+		 */
+		Node middle = new Node();
+		middle.setValue(5);
+		
+		/***
+		 * Link first and middle Node
+		 * 3 | null --> 5 | null 
+		 */
+		first.setNext(middle);
+		
+		/***
+		 * Last Node
+		 * 3 | null --> 5 | null 10 |null 
+		 */
+		Node last = new Node();
+		last.setValue(7);
+		
+		/***
+		 * Link middle and last Node
+		 * 3 | null --> 5 | null --> 10 |null 
+		 */
+		middle.setNext(last);
+	//Iterate through the node
+		PrintList(first);
 	
+	}
+	/*
+	 * Static function to iterate through the node
+	 */
+	private static void PrintList(Node node){
+		
+		while(node!=null){
+			System.out.println(node.getValue());
+			node=node.getNext();
+		}
 	}
 	
 }
---
+++ src/Node.java
@@ -0,0 +1,20 @@
+
+public class Node {
+	private int value;
+	private Node next;
+		
+	public void setNext(Node newValue){
+		next=newValue;
+	}
+	public void setValue(int newValue){
+		value=newValue;
+	}
+	public Node getNext(){
+		return next;
+	}
+	public int getValue(){
+		return value;
+	}
+	
+
+}
