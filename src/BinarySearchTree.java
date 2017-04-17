cadf712d27a8fb2ad00682fafb0bc9042e5e9983
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-04-16 23:13:03 -0700

    Binary tree insert functionality has been added

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,161 +1,126 @@
-
-public class BinarySearchTree {
-	public static  Node root;
-	public BinarySearchTree(){
-		this.root = null;
-	}
-	
-	public boolean find(int id){
-		Node current = root;
-		while(current!=null){
-			if(current.data==id){
-				return true;
-			}else if(current.data>id){
-				current = current.left;
-			}else{
-				current = current.right;
-			}
-		}
-		return false;
-	}
-	public boolean delete(int id){
-		Node parent = root;
-		Node current = root;
-		boolean isLeftChild = false;
-		while(current.data!=id){
-			parent = current;
-			if(current.data>id){
-				isLeftChild = true;
-				current = current.left;
-			}else{
-				isLeftChild = false;
-				current = current.right;
-			}
-			if(current ==null){
-				return false;
-			}
-		}
-		//if i am here that means we have found the node
-		//Case 1: if node to be deleted has no children
-		if(current.left==null && current.right==null){
-			if(current==root){
-				root = null;
-			}
-			if(isLeftChild ==true){
-				parent.left = null;
-			}else{
-				parent.right = null;
-			}
-		}
-		//Case 2 : if node to be deleted has only one child
-		else if(current.right==null){
-			if(current==root){
-				root = current.left;
-			}else if(isLeftChild){
-				parent.left = current.left;
-			}else{
-				parent.right = current.left;
-			}
-		}
-		else if(current.left==null){
-			if(current==root){
-				root = current.right;
-			}else if(isLeftChild){
-				parent.left = current.right;
-			}else{
-				parent.right = current.right;
-			}
-		}else if(current.left!=null && current.right!=null){
-			
-			//now we have found the minimum element in the right sub tree
-			Node successor	 = getSuccessor(current);
-			if(current==root){
-				root = successor;
-			}else if(isLeftChild){
-				parent.left = successor;
-			}else{
-				parent.right = successor;
-			}			
-			successor.left = current.left;
-		}		
-		return true;		
-	}
-	
-	public Node getSuccessor(Node deleleNode){
-		Node successsor =null;
-		Node successsorParent =null;
-		Node current = deleleNode.right;
-		while(current!=null){
-			successsorParent = successsor;
-			successsor = current;
-			current = current.left;
-		}
-		//check if successor has the right child, it cannot have left child for sure
-		// if it does have the right child, add it to the left of successorParent.
-//		successsorParent
-		if(successsor!=deleleNode.right){
-			successsorParent.left = successsor.right;
-			successsor.right = deleleNode.right;
-		}
-		return successsor;
-	}
-	public void insert(int id){
-		Node newNode = new Node(id);
-		if(root==null){
-			root = newNode;
-			return;
-		}
-		Node current = root;
-		Node parent = null;
-		while(true){
-			parent = current;
-			if(id<current.data){				
-				current = current.left;
-				if(current==null){
-					parent.left = newNode;
-					return;
-				}
-			}else{
-				current = current.right;
-				if(current==null){
-					parent.right = newNode;
-					return;
-				}
-			}
-		}
-	}
-	public void display(Node root){
-		if(root!=null){
-			display(root.left);
-			System.out.print(" " + root.data);
-			display(root.right);
-		}
-	}
-	public static void main(String arg[]){
-		BinarySearchTree b = new BinarySearchTree();
-		b.insert(3);b.insert(8);
-		b.insert(1);b.insert(4);b.insert(6);b.insert(2);b.insert(10);b.insert(9);
-		b.insert(20);b.insert(25);b.insert(15);b.insert(16);
-		System.out.println("Original Tree : ");
-		b.display(b.root);		
-		System.out.println("");
-		System.out.println("Check whether Node with value 4 exists : " + b.find(4));
-		System.out.println("Delete Node with no children (2) : " + b.delete(2));		
-		b.display(root);
-		System.out.println("\n Delete Node with one child (4) : " + b.delete(4));		
-		b.display(root);
-		System.out.println("\n Delete Node with Two children (10) : " + b.delete(10));		
-		b.display(root);
-	}
-}
-
-class Node{
+//Class for creating Node
+class Node {
 	int data;
 	Node left;
-	Node right;	
+	Node right;
 	public Node(int data){
-		this.data = data;
-		left = null;
-		right = null;
+		this.data=data;
+		left=null;
+		right=null;
 	}
+	
+}
+
+//Create binary search tree
+public class BinarySearchTree{
+	//root node
+	public static Node root;
+	//constructor which assigns root to null
+	public BinarySearchTree(){
+		this.root=null;
+	}
+	/*
+	 * 1. We need to start from the root and compare the data with n
+	 * 2. If root.data is greater than n then we need to go to left
+	 * 3. If root.data is lesser than n then we need to go to right
+	 * 4. If root.data is equal to n then we were able to find the data else return false
+	 */
+	//function to find node in a tree
+	public boolean find(int id){
+	//set the current node to the root
+	Node current=root;
+	//Traverse the root till we find n
+		 while (current!=null)
+	 {
+		 //if data is the search element then return true
+		 if(current.data==id){
+			 return true;
+		 }
+		 //If the current data of the node is greater than the search element the traverse to the left
+		 else if (current.data>id)
+		 {
+			 current=current.left;
+		 }
+		 else
+		 {
+			 current=current.right;
+		 }
+		 
+	 }
+	 return false;
+	}
+	/**
+	 * We need to set current = root
+	 * Compare root.data. If root.data is greater than n we need to traverse to the left
+	 * If root.data is lesser than n then we traverse to the right
+	 * If current is null.Then we have reached the root node. We need to insert the node with the help of the parent node
+	 * @param id
+	 */
+	//function to insert into a tree
+	public void insert(int id)
+	{
+		//Create a new instance of a node
+		 Node newNode= new Node(id);
+		
+	     // Binary tree is empty
+		 if(root==null)
+		 {
+			 root=newNode;
+			 return;
+		 }
+		 //Set current node are root
+		 Node current =root;
+		 Node parent =null;
+		 
+		 //While loop till we find the leaf node and insert the new node
+		 while(true)
+		 {
+			 //set parent to current
+			 parent=current;
+			 //Check if the current is greater than the id
+			 if(id >current.data)
+			 {
+				 current=current.left;
+				 //This would be the leaf node
+				 if(current==null)
+				 {
+					 parent.left=newNode;
+				    return;
+				 }
+			 }
+				 else
+				 {
+					 current=current.right;
+					 if(current==null)
+					 {
+						 parent.right=newNode;
+						 return;
+					 }
+				 }
+			 }
+		
+		 }
+	/**
+	 * Recursion function to display content from the Binary Search tree.
+	 * @param arg
+	 */
+ public void display(Node root){
+	 //Check if the tree is empty
+	 if(root!=null)
+	 {
+		 display(root.left);
+		 System.out.print(" " +root.data);
+		 display(root.right);
+	 }
+	 
+ }
+
+//Main function
+public static void main(String arg[]){
+	
+	BinarySearchTree b=new BinarySearchTree();
+	System.out.println("Insert");
+	b.insert(10); b.insert(2);  b.insert(7);b.insert(18);
+	b.display(root);
+    }
 }
